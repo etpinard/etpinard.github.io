0cfa1f7ff6a6f1263e4ef488fb984d8386b054c8
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2017-07-03 20:19:33 -0400

    try fetching master

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,10 +2,11 @@
 
 DATE=$(date)
 
+git fetch master
 git checkout master
 git merge source
 npm install
 npm run build
-git add --all
+git add build
 git commit -m "$DATE build"
 git push origin master
