424a029c64535a0534f4850c3ac6c92f668cb235
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2017-07-03 20:32:13 -0400

    merge w/o commit & add --all

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,9 +4,11 @@ DATE=$(date)
 
 git fetch origin master:master
 git checkout master
-git merge source
+git merge source --no-commit
+
 npm install
 npm run build
-git add build
+
+git add --all
 git commit -m "$DATE build"
 git push origin master
